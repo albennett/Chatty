a2309b5e2525c98f873f11dc41236dda862f4bfe
bbleds <bledsoedesigns@gmail.com>
2015-10-19 15:41:57 -0500

    Added editing functionality

--- entermessage.js
+++ entermessage.js
@@ -4,14 +4,28 @@ var messageInput = document.getElementById("message-input");
 //Get message holder 
 var messageHolder = document.getElementById("message-holder");
 
+//bolean if edit mode is on
+var IAmEditing = false;
+
 function postMessage(e) {
 	if(e.keyCode === 13){
-	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'>"+messageInput.value+"</p>"+"<button class='bRemove'>Delete</button></div>";
+			if(IAmEditing === true){
+				document.getElementsByClassName("pToAdd")[0].innerHTML = document.getElementById("message-input").value;
+				IAmEditing = false;
+				console.log("I am editing", IAmEditing);
+				document.getElementsByClassName("pToAdd")[0].classList.remove("pToAdd");
+		} else {
+	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'>"+messageInput.value+"</p>"+"<button class='bRemove'>Delete</button><button class='edit'>Edit</button></div>";
 	messageInput.value = "";
-	}
+		}
+	}	
 	var buttons = messageHolder.getElementsByClassName("bRemove");
+	var edits = messageHolder.getElementsByClassName("edit");
 	for(var i = 0; i < buttons.length; i++){
 		buttons[i].addEventListener("click", getParent);
+	}
+	for(var i = 0; i < buttons.length; i++){
+		edits[i].addEventListener("click", editMessage);
 	}
 	document.getElementById("clear_box").disabled=false;
 }
@@ -20,6 +34,12 @@ function postMessage(e) {
 function clearBox(){
 	messageHolder.innerHTML = "";
 	document.getElementById("clear_box").disabled=true;
+}
+
+function editMessage(){
+	document.getElementById("message-input").value = this.parentNode.childNodes[0].innerHTML;
+	IAmEditing = true;
+	this.parentNode.childNodes[0].classList.add('pToAdd');
 }
 
 function getParent(){
