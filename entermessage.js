801f5cc49534081adc26a253706ccf3dc1f5ec01
albennett <amyleeannebennett@gmail.com>
2015-10-19 15:43:39 -0500

    working on edit

--- entermessage.js
+++ entermessage.js
@@ -3,10 +3,14 @@ var messageInput = document.getElementById("message-input");
 
 //Get message holder 
 var messageHolder = document.getElementById("message-holder");
-
+var iAmEditing = false;
 function postMessage(e) {
 	if(e.keyCode === 13){
-	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'>"+messageInput.value+"</p>"+"<button class='bRemove'>Delete</button></div>";
+		if (iAmEditing === true) {
+			document.getElementsByClassName("addP")[0].innerHTML = document.getElementById("messageInput").value;
+		}
+	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'>"+messageInput.value+"</p>"+"<button class='bRemove'>Delete</button></div>" 
+	+ "<button class='edit'>Edit</button></div>" ;
 	messageInput.value = "";
 	}
 	var buttons = messageHolder.getElementsByClassName("bRemove");
@@ -14,12 +18,23 @@ function postMessage(e) {
 		buttons[i].addEventListener("click", getParent);
 	}
 	document.getElementById("clear_box").disabled=false;
+	var edit = messageHolder.getElementsByClassName("edit");
+	for (var i = 0; i < edit.length; i++) {
+		edit[i].addEventListener("click", editMessage);
+	}
 }
 
+function editMessage() {
+	console.log("I edit!");
+	console.log(this.parentNode.childNodes[0]);
+	var theChild = this.parentNode.childNodes[0].classList.add("addP");
+
+}
 
 function clearBox(){
 	messageHolder.innerHTML = "";
 	document.getElementById("clear_box").disabled=true;
+	console.log(this + "This is what we are targeting");
 }
 
 function getParent(){
@@ -36,6 +51,8 @@ document.getElementById("clear_box").addEventListener("click", clearBox);
 // console.log(messageHolder.getElementsByTagName("button"));
 
 var buttons = messageHolder.getElementsByClassName("bRemove");
+var edit = messageHolder.getElementsByClassName("edit");
+console.log(edit);
 console.log(buttons);
 // buttons.addEventListener("click", testSay);
 // buttons.addEventListener("click", testSay);
