89c4fc6748cdf6b9bcc0e8fdc7f610eb7956b2df
albennett <amyleeannebennett@gmail.com>
2015-10-22 15:20:41 -0500

    changing to jquery post message

--- changetheme.js
+++ changetheme.js
@@ -1,30 +1,38 @@
-var optionChosen = document.getElementsByName("checked-input");
-var choseDarkTheme = document.getElementById("dark-input");
-var choseLargeText = document.getElementById("large-text");
-var messageHolder = document.getElementById("message-holder");
+$(document).ready(function() {
+
+var optionChosen = $("[name='checked-input']");
+console.log("optionChosen", optionChosen);
+var choseDarkTheme = $("#dark-input");
+var choseLargeText = $("#large-text");
+var messageHolder = $("#message-holder");
 
 function changeDark (event) {
  if (choseDarkTheme.checked) {
-  document.body.classList.toggle("dark-styles");
-  document.body.style.color = "white";
-  console.log("hey");
+	 $("body").toggleClass("dark-styles");
+	  $("body").css("color", "white");
+	  console.log("hey");
  } else {
- 	document.body.classList.toggle("dark-styles");
- 	document.body.style.color = "black";
+ 	$("body").toggleClass("dark-styles");
+ 	$("body").css("color", "black");
  }
  console.log("hey");
 }
 
 function changeSize(event) {
   if(choseLargeText.checked) {
-  document.getElementById("message-holder").classList.toggle("font-sizes");
+  $("#message-holder").toggleClass("font-sizes");
   // console.log("hello");
  } else {
- 	document.getElementById("message-holder").classList.toggle("font-sizes");
+ 	$("#message-holder").toggleClass("font-sizes");
  }
 }
 
-document.getElementById("dark-input").addEventListener("change", changeDark);
+$("#dark-input").click(function(){ 
+	changeDark();
+});
 
- // document.getElementById("message-holder").innerHTML = "<div id='messageid'>hello</div>";
- document.getElementById("large-text").addEventListener("change", changeSize);
+ // $("message-holder").innerHTML = "<div id='messageid'>hello</div>";
+ $("#large-text").click( changeSize);
+
+
+});
--- entermessage.js
+++ entermessage.js
@@ -1,102 +1,27 @@
 //Get input element 
-var messageInput = document.getElementById("message-input");
+/*var messageInput = $("#message-input");
 
 //Get message holder 
-var messageHolder = document.getElementById("message-holder");
-
+var messageHolder = $("#message-holder");
+*/
 //bolean if edit mode is on
 var IAmEditing = false;
 
 
 
 function postMessage(e) {
-
-	//create time Stamp 
-	var theDate = new Date;
-
-	//loop over radio buttons
-	for(var i = 0; i < users.names.length; i ++){
-
-		//if button is checked
-		if(document.getElementsByClassName("users")[i].checked === true){
-
-			//get the value of the radio button and set it equal to theOneChecked variable
-			var theOneChecked = document.getElementsByClassName("users")[i].value;
-			console.log(theOneChecked);
-		}
-	}
-
-	//if enter key is presses
-	if(e.keyCode === 13){
-
-			//if edit mode is on (true)
-			if(IAmEditing === true){
-
-				//get element with class name pToAdd
-				document.getElementsByClassName("pToAdd")[0].innerHTML = document.getElementById("message-input").value;
-
-				// IAmEditing to false
-				IAmEditing = false;
-
-				console.log("I am editing", IAmEditing);
-
-				//remove edit class
-				document.getElementsByClassName("pToAdd")[0].classList.remove("pToAdd");
-
-				//if user is selected, put value of that radio button before the input message
-		} else if(theOneChecked !== undefined){
-			//output with radio value
-	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'><span>"+theOneChecked+":</span> "+messageInput.value+"</p>"+
-	"<button class='bRemove'>Delete</button><button class='edit'>Edit</button><span class='date'>message sent on: "+theDate+"</span></div></div>";
-			//clear input
-	messageInput.value = "";
-			} else {
-			//output with username value
-	messageHolder.innerHTML += "<div class='mHolder'><p class='message-Item'>"+messageInput.value+"</p>"+"<button class='bRemove'>Delete</button><button class='edit'>Edit</button><span class='date'>message sent on: "+theDate+"</span></div> ";
-			//clear value
-	messageInput.value = "";
-		}
-	}	
-	//get all buttons with delete class
-	var buttons = messageHolder.getElementsByClassName("bRemove");
-
-	//get all buttons with edit class
-	var edits = messageHolder.getElementsByClassName("edit");
-
-	//for length of button elements
-	for(var i = 0; i < buttons.length; i++){
-		//connected them with event lister for delete function
-		buttons[i].addEventListener("click", getParent);
-	}
-
-	//for length of button elements
-	for(var i = 0; i < buttons.length; i++){
-
-		//connect with edit function
-		edits[i].addEventListener("click", editMessage);
-	}
-
-	//enable clear button
-	document.getElementById("clear_box").disabled=false;
-
-	//if the length of the divs inside of the message holder is greater than 20
-	if(document.getElementById("message-holder").getElementsByClassName("mHolder").length > 20){
-
-		//remove te first child node
-		document.getElementById("message-holder").removeChild(document.getElementById("message-holder").childNodes[0]);
-	}
 }
 
 //function to clear message holder
 function clearBox(){
-	messageHolder.innerHTML = "";
-	document.getElementById("clear_box").disabled=true;
+	$("#message-holder").innerHTML = "";
+	$("#clear_box").disabled=true;
 }
 
 //function for editing messages when edit button clicked
 function editMessage(){
 	//set value of input to first childnode
-	document.getElementById("message-input").value = this.parentNode.childNodes[0].innerHTML;
+	$("#message-input").value = this.parentNode.childNodes[0].innerHTML;
 
 	//set iAmEditing to true
 	IAmEditing = true;
@@ -116,12 +41,92 @@ function getParent(){
 
 
 //run function of enter key press
-messageInput.addEventListener("keyup", postMessage);
+$("#message-input").keypress(function(e){
+	console.log("e", e);
+	//create time Stamp 
+	var theDate = new Date;
+
+	//loop over radio buttons
+	for(var i = 0; i < users.names.length; i++){
+
+		//if button is checked
+		if($(".users")[i].checked === true){
+			console.log("theonecheck", theOneChecked);
+
+			//get the value of the radio button and set it equal to theOneChecked variable
+			var theOneChecked = $(".users")[i].value;
+			console.log(theOneChecked);
+		}
+	}
+
+	//if enter key is presses
+	if(e.which === 13){
+		console.log("insideife", e);
+
+			//if edit mode is on (true)
+			if(IAmEditing === true){
+
+				//get element with class name pToAdd
+				$(".pToAdd")[0].html($("#message-input").val());
+
+				// IAmEditing to false
+				IAmEditing = false;
+
+				console.log("I am editing", IAmEditing);
+
+				//remove edit class
+				$(".pToAdd")[0].classList.remove("pToAdd");
+
+				//if user is selected, put value of that radio button before the input message
+		} else if(theOneChecked !== undefined){
+			//output with radio value
+	$("#message-holder").append("<div class='mHolder'><p class='message-Item'><span>"+theOneChecked+":</span> "+$("#message-input").val() +"</p>"+
+	"<button class='bRemove'>Delete</button><button class='edit'>Edit</button><span class='date'>message sent on: "+theDate+"</span></div></div>");
+			//clear input
+	$("#message-input").val(""); 
+			} else {
+			//output with username value
+	$("#message-holder").append("<div class='mHolder'><p class='message-Item'>"+$("#message-input").val()+"</p>"+"<button class='bRemove'>Delete</button><button class='edit'>Edit</button><span class='date'>message sent on: "+theDate+"</span></div> ");
+			//clear value
+	$("#message-input").val("");
+		}
+	}	
+	//get all buttons with delete class
+	var buttons = $("#message-holder .bRemove");
+
+	//get all buttons with edit class
+	var edits = $("#message-holder .edit");
+
+	//for length of button elements
+	for(var i = 0; i < buttons.length; i++){
+		//connected them with event lister for delete function
+		buttons[i].addEventListener("click", getParent);
+	}
+
+	//for length of button elements
+	for(var i = 0; i < buttons.length; i++){
+
+		//connect with edit function
+		edits[i].addEventListener("click", editMessage);
+	}
+
+	//enable clear button
+	$("#clear_box").disabled=false;
+
+	//if the length of the divs inside of the message holder is greater than 20
+	if($("#message-holder .mHolder").length > 20){
+
+		//remove te first child node
+		$("#message-holder").removeChild($("#message-holder").childNodes[0]);
+	}
+});
 //add event listener for clear function
-document.getElementById("clear_box").addEventListener("click", clearBox);
+$("#clear_box").click(function() {
+	clearBox();
+});
 // console.log(messageHolder.getElementsByTagName("button"));
 
-var buttons = messageHolder.getElementsByClassName("bRemove");
+var buttons = $("#message-input .bRemove");
 console.log(buttons);
 // buttons.addEventListener("click", testSay);
 // buttons.addEventListener("click", testSay);
@@ -137,12 +142,12 @@ var users = {
 //loop over users to created radio buttons
 for (var i = 0; i< users.names.length; i++){
 	//output radio buttons
-	document.getElementById("radioHolder").innerHTML += users.names[i]+"<input class='users' name='user' type='radio' value="+users.names[i]+">";
+	$("#radioHolder").append(users.names[i]+"<input class='users' name='user' type='radio' value="+users.names[i]+">");
 }
 
-// console.log(document.getElementsByClassName("users"));
+// console.log($(".users"));
 
-// console.log(document.getElementById("message-holder").getElementsByClassName("mHolder").length);
+// console.log($("#message-holder").getElementsByClassName("mHolder").length);
 
 
 
